019ab0dd71cc38c3dd2c4ca97f53544f374176ff
kalinatringas <151895163+kalinatringas@users.noreply.github.com>
2025-01-03 23:34:05 -0500

    this should make it work now

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  root: './',  // Ensure the root is correctly set to your project root
+  build: {
+    outDir: 'build',  // Output directory for the build
+  },
   base: '/Gainesville-Events/'
 })
