ce7dfefe51ab12d0ff57b48e48323671eec179f8
kalinatringas <151895163+kalinatringas@users.noreply.github.com>
2025-01-03 23:45:03 -0500

    removed index from src

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,12 @@ export default defineConfig({
   root: './',  // Ensure the root is correctly set to your project root
   build: {
     outDir: 'build',  // Output directory for the build
+    rollupOptions: {
+      input: './index.html' // specify the entry file
+    }
   },
   base: '/Gainesville-Events/'
 })
+
+
+// vite.config.js
